b15022651891ad649210d28ccdff790e1de00639
Hector Ortega <hector.oj@gmail.com>
2016-12-16 11:46:38 -0500

    Use Dial verb on outbound TwiML:
     - Add sales phone number field to view
     - Send sales phone number on the callback URL and dial it on outbound TwiML
     - Update tests

--- src/main/java/com/twilio/clicktocall/twilio/TwiMLUtil.java
+++ src/main/java/com/twilio/clicktocall/twilio/TwiMLUtil.java
@@ -1,14 +1,18 @@
 package com.twilio.clicktocall.twilio;
 
+import com.twilio.twiml.Dial;
+import com.twilio.twiml.Number;
 import com.twilio.twiml.Say;
 import com.twilio.twiml.TwiMLException;
 import com.twilio.twiml.VoiceResponse;
 
 public class TwiMLUtil {
 
-    public static String buildVoiceResponse(String say) throws TwiMLException {
+    public static String buildVoiceResponseAndDial(String say, String salesPhone) throws TwiMLException {
+        Number number = new Number.Builder(salesPhone).build();
         return new VoiceResponse.Builder()
                 .say(new Say.Builder(say).build())
+                .dial(new Dial.Builder().number(number).build())
                 .build()
                 .toXml();
     }
--- src/main/java/com/twilio/clicktocall/twilio/TwilioLine.java
+++ src/main/java/com/twilio/clicktocall/twilio/TwilioLine.java
@@ -1,8 +1,12 @@
 package com.twilio.clicktocall.twilio;
 
+import com.twilio.clicktocall.CallException;
 import com.twilio.http.TwilioRestClient;
 import com.twilio.rest.api.v2010.account.CallCreator;
 import com.twilio.type.PhoneNumber;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class TwilioLine {
     private String twilioNumber;
@@ -14,7 +18,11 @@ public class TwilioLine {
     }
 
     public void call(final String phoneNumber, final String responseUrl)  {
-        CallCreator callCreator = new CallCreator(new PhoneNumber(phoneNumber), new PhoneNumber(twilioNumber), responseUrl);
-        callCreator.create(restClient);
+        try {
+            CallCreator callCreator = new CallCreator(new PhoneNumber(phoneNumber), new PhoneNumber(twilioNumber), new URI(responseUrl));
+            callCreator.create(restClient);
+        } catch (URISyntaxException e) {
+            throw new CallException(e);
+        }
     }
 }
